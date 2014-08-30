1f1f8856444241a7b5d6e71fde0d33466cbf4269
Scott Frederick <scottyfred@gmail.com>
2014-08-30 01:57:24 -0500

    Changed default response format from hal+json to json.

--- src/main/java/com/example/cities/Application.java
+++ src/main/java/com/example/cities/Application.java
@@ -12,7 +12,6 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguratio
 @ComponentScan
 @EnableAutoConfiguration
 @EnableJpaRepositories
-@Import(RepositoryRestMvcConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
