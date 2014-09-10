e307c8f3aed9e00f0979ec88695f40e95dee9d36
Scott Frederick <scottyfred@gmail.com>
2014-09-09 21:54:29 -0500

    Added postgres driver to pom.xml.

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,7 @@
-heroku create spring-boot-cities --stack cedar --buildpack http://github.com/heroku/heroku-buildpack-java.git
+heroku create cities-s12gx --stack cedar --buildpack http://github.com/heroku/heroku-buildpack-java.git
 APP_PATH=`heroku config:get PATH`
 heroku config:set PATH=/app/.jdk/bin:$APP_PATH
+heroku config:set SPRING_CLOUD_APP_NAME=spring-boot-cities
+heroku config:set SPRING_PROFILES_ACTIVE=cloud
 heroku addons:add heroku-postgresql:hobby-basic
 git push heroku json-heroku:master
