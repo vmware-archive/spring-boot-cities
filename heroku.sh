f1944f8199ab32ba4ae93fd840c63d9a3503a9bd
Scott Frederick <scottyfred@gmail.com>
2014-09-09 22:13:20 -0500

    Shrunk import file.

--- heroku.sh
+++ heroku.sh
@@ -3,5 +3,5 @@ APP_PATH=`heroku config:get PATH`
 heroku config:set PATH=/app/.jdk/bin:$APP_PATH
 heroku config:set SPRING_CLOUD_APP_NAME=spring-boot-cities
 heroku config:set SPRING_PROFILES_ACTIVE=cloud
-heroku addons:add heroku-postgresql:hobby-basic
+heroku addons:add heroku-postgresql:hobby-dev
 git push heroku json-heroku:master
