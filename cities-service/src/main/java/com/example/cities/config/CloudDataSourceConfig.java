927ad3b2cdd432b30ff62d4a1965f6ef711d2728
Scott Frederick <scottyfred@gmail.com>
2014-09-12 15:21:05 -0500

    Changed RESTRepository default metadata format from hal+json to json. Adjusted demo script to match.

--- cities-service/src/main/java/com/example/cities/config/CloudDataSourceConfig.java
+++ cities-service/src/main/java/com/example/cities/config/CloudDataSourceConfig.java
@@ -1,8 +1,6 @@
 package com.example.cities.config;
 
 import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.cloud.service.PooledServiceConnectorConfig;
-import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,11 +12,6 @@ import javax.sql.DataSource;
 public class CloudDataSourceConfig extends AbstractCloudConfig {
     @Bean
     public DataSource dataSource() {
-        PooledServiceConnectorConfig.PoolConfig poolConfig = new PooledServiceConnectorConfig.PoolConfig(20, 200);
-        DataSourceConfig.ConnectionConfig connectionConfig =
-                new DataSourceConfig.ConnectionConfig("sessionVariables=sql_mode='ANSI';characterEncoding=UTF-8");
-        DataSourceConfig serviceConfig =
-                new DataSourceConfig(poolConfig, connectionConfig);
-        return connectionFactory().dataSource("cities-db", serviceConfig);
+        return connectionFactory().dataSource();
     }
 }
