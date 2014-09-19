e4b6123ab5f3f976585a86f9de2a296cb1749a52
Scott Frederick <scottyfred@gmail.com>
2014-09-19 17:01:27 -0500

    Changed client lib to support unmarshalling HAL hypermedia. Reverted service back to generating HAL hypermedia. Changed client lib to support unmarshalling HAL hypermedia. Reverted service back to generating HAL hypermedia.

--- cities-client/src/main/java/com/example/cities/client/CityRepositoryFactory.java
+++ cities-client/src/main/java/com/example/cities/client/CityRepositoryFactory.java
@@ -1,14 +1,21 @@
 package com.example.cities.client;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
+import org.springframework.hateoas.hal.Jackson2HalModule;
 
 import java.net.URL;
 
 public class CityRepositoryFactory {
     public CityRepository create(String url) {
+        ObjectMapper mapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new Jackson2HalModule());
+
         return Feign.builder()
-                .decoder(new JacksonDecoder())
+                .decoder(new JacksonDecoder(mapper))
                 .target(CityRepository.class, url);
     }
 
--- cities-service/src/main/java/com/example/cities/Application.java
+++ cities-service/src/main/java/com/example/cities/Application.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
