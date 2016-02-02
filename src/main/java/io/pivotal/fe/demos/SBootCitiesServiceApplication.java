da075a80e5d474cc59f07afd8ae32d86615662be
Sufyaan Kazi <skazi@pivotal.io>
2016-02-02 14:11:12 +0000

    Correct content

---
+++ src/main/java/io/pivotal/fe/demos/SBootCitiesServiceApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.fe.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBootCitiesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SBootCitiesServiceApplication.class, args);
+    }
+}
